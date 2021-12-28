3224d18de5917c22699d1b3fedf9feb6779e10a0
Jack1641 <ngkxiang@gmail.com>
2021-12-28 14:44:46 +0800

    .

---
+++ AttemptQuiz.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AttemptQuiz.aspx.cs" Inherits="Quiz_Web_App.AttemptQuiz" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div>
+            <center>
+                <asp:Repeater ID="Repeater1" runat="server">
+                    <ItemTemplate>
+                        <table>
+                            <tr>
+                                <td>
+                                    <%#Eval("Ques_id") %> : <%#Eval("Title") %> (Score: <%#Eval("Score") %>)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <asp:RadioButtonList ID="rbl1" runat="server">
+                                        <asp:ListItem Value="Op1" Text='<%#Eval("Option1") %>' GroupName="q1"/>
+                                        <asp:ListItem Value="Op2" Text='<%#Eval("Option2") %>' GroupName="q1"/>
+                                        <asp:ListItem Value="Op3" Text='<%#Eval("Option3") %>' GroupName="q1"/>
+                                        <asp:ListItem Value="Op4" Text='<%#Eval("Option4") %>' GroupName="q1"/>
+                                    </asp:RadioButtonList>
+                                    
+                                    <br />
+                                    <asp:Label ID="LabCorrectAns" runat="server" Text='<%#Eval("AnsId") %>' Visible="false"></asp:Label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <asp:Label ID="LabUserSelectedOption" runat="server" Text=""></asp:Label>
+                                </td>
+                            </tr>
+                        </table>
+                    </ItemTemplate>
+                </asp:Repeater>
+                <asp:Button ID="BtnSubmit" runat="server" Text="Submit" OnClick="BtnSubmit_Click"/>
+            </center>
+
+        </div>
+</asp:Content>
---
+++ AttemptQuiz.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Quiz_Web_App
+{
+    public partial class AttemptQuiz : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string mainconn = ConfigurationManager.ConnectionStrings["QuizWebsiteDBConnectionString"].ConnectionString;
+                SqlConnection sqlconn = new SqlConnection(mainconn);
+                string sqlquery = "SELECT q.Ques_id, q.Title, q.Score, o.Option1, o.Option2, o.Option3, o.Option4, a.AnsId " +
+                                  "FROM Quiz_question q " +
+                                  "INNER JOIN Quiz_option AS o ON q.Ques_id = o.Ques_id " +
+                                  "INNER JOIN Quiz_ans AS a ON q.Ques_id = a.QuesId";
+                sqlconn.Open();
+                SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+                SqlDataAdapter sdr = new SqlDataAdapter(sqlcomm);
+                DataTable dt = new DataTable();
+                sdr.Fill(dt);
+                Repeater1.DataSource = dt;
+                Repeater1.DataBind();
+                sqlconn.Close();
+            }
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            string selected = string.Empty;
+            if (rbl1.SelectedIndex > 0)
+            {
+                if (rbl1.SelectedValue == "Op1")
+                {
+                    selected = "1";
+                }
+            }
+            else
+            {
+                selected = "0";
+            }
+            string constr = ConfigurationManager.ConnectionStrings["QuizWebsiteDBConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Student_ans(AnsId) VALUES(@AnsId)"))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@AnsId", selected);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+    }
+}
