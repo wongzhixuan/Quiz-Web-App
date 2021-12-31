f7f1538e5b755e0fc5fc6d5a972a49c4df67fab1
Jack1641 <ngkxiang@gmail.com>
2021-12-31 22:21:46 +0800

    .

--- AttemptQuiz.aspx.designer.cs
+++ AttemptQuiz.aspx.designer.cs
@@ -33,15 +33,6 @@ namespace Quiz_Web_App
         protected global::System.Web.UI.WebControls.GridView GridView1;
 
         /// <summary>
-        /// SelectedAns control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label SelectedAns;
-
-        /// <summary>
         /// BtnSubmit control.
         /// </summary>
         /// <remarks>
